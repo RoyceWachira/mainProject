cb85b97464f16568cde362f77f7b72846eb58b1b
Marvin Wachira <marvinwachira6@gmail.com>
2023-05-18 16:46:16 +0300

    Updates

---
+++ getChamaMembers.php
@@ -0,0 +1,40 @@
+<?php
+
+// Start the session
+session_start();
+$sid=session_id();
+// Path to access the functions in DbConnect
+require_once '../API/DbOperations.php';
+
+$response = array();
+
+// Check for correct request method
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+        $db = new DbOperation();
+
+        $chamaId = $_GET['chama_id'];
+
+        if ($db) {
+            $members = $db->getChamaMembers($chamaId);
+
+            if ($members) {
+                $response['error'] = false;
+                $response['members'] = $members;
+            } else {
+                $response['error'] = true;
+                $response['message'] = "Error fetching members";
+            }
+        } else {
+            $response['error'] = true;
+            $response['message'] = "Database connection error";
+        }
+    
+} else {
+    $response['error'] = true;
+    $response['message'] = "Invalid Request";
+}
+
+echo json_encode($response);
+
+?>
